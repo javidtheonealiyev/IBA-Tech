9eabd0a20d5f973d5cc2eb0f859ee03dc88b4539
Cavid. A <justforouter@gmail.com>
2021-02-08 12:25:45 +0400

    adding 2nd line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Testing commit-push");
+        System.out.println("Connection successful");
     }
 }
