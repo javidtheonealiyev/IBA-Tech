822b83e8626705fd868c42fcd46e1a52a53ef2e4
Cavid. A <justforouter@gmail.com>
2021-02-08 16:28:45 +0400

    for fun

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Testing commit-push");
         System.out.println("Connection successful");
         System.out.println("Adding the 3rd line, time 13:25 Local");
+        
     }
 }
