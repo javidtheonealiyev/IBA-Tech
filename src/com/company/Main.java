e197602df87e4a13533272b206973219fd289f50
Cavid. A <justforouter@gmail.com>
2021-02-08 13:26:08 +0400

    3rd line time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Testing commit-push");
         System.out.println("Connection successful");
+        System.out.println("Adding the 3rd line, time 13:25 Local");
     }
 }
